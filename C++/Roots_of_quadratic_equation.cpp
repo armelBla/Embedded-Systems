623abe90b9648a1507de4aa9a4cbc6b373cb1d31
Armel_TechAlchemy <110736523+armelBla@users.noreply.github.com>
2023-12-18 10:49:17 -0500

    Update Roots_of_quadratic_equation.cpp

--- C++/Roots_of_quadratic_equation.cpp
+++ C++/Roots_of_quadratic_equation.cpp
@@ -12,8 +12,8 @@ int main()
     cout << "Enter the three variable a, b, c\n";
     cin >> a >> b >> c;
 
-    rootsOne = ((-b) + sqrt(pow(b, 2) - (4 * a * c))) / 2 * a;
-    rootsTw = ((-b) - sqrt(pow(b, 2) - (4 * a * c))) / 2 * a;
+    rootsOne = ((-b) + sqrt(pow(b, 2) - (4 * a * c))) / (2 * a);
+    rootsTw = ((-b) - sqrt(pow(b, 2) - (4 * a * c))) / (2 * a);
 
     cout << "the first solution is " << rootsOne;
     cout << "the second solution us " << rootsTw;
